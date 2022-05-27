4f3ffde462f0eb93c72ab5fad377f5ef36bc1b13
Spyros Bekiaris <spirosbekos@gmail.com>
2022-05-27 13:57:26 +0300

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+// on ready
+$(document).ready(function(){
+
+	// initialize
+	$('.slick').slick({
+		infinite: true,
+		slidesToShow: 1,
+		autoplaySpeed: 6000,
+		slidesToScroll: 1,
+		arrows: false,
+        dots: true
+	});
+});
